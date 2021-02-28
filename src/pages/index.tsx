e8ad3cebcc5afb5e93675d51b129a32286f1147e
WesRaiuga <weslley_wenny@hotmail.com>
2021-02-27 21:33:39 -0300

    NLW Aula 3: Context API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
+import Head from 'next/head';
+import styles from '../styles/pages/Home.module.css';
+
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
-
-import Head from 'next/head';
-
-import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -25,7 +25,7 @@ export default function Home() {
         </div>
 
         <div>
-
+          <ChallengeBox />
         </div>
 
       </section>
